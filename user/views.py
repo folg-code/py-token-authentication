9b4dcc509d94d06cddb8a9b181e5d8ed95fc312c
filip <filip1folga@gmail.com>
2025-10-01 15:18:39 +0200

    create view, serializerm, add urls for user.

--- user/views.py
+++ user/views.py
@@ -1,1 +1,7 @@
-# write your code here
+from rest_framework.generics import CreateAPIView
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(CreateAPIView):
+    serializer_class = UserSerializer
